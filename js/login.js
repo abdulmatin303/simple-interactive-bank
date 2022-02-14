ef4dd3a5255cf89474b7468564a7599ed8b727cb
amr303 <amr.abdulmatin@gmail.com>
2022-02-15 05:55:10 +0600

    simple bank upload

---
+++ js/login.js
@@ -0,0 +1,12 @@
+// get user email
+document.getElementById('login-submit').addEventListener('click', function(){
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    // check email and password
+    if(userEmail == 'sontan@baap.com' && userPassword == 'secret'){
+        window.location.href = 'banking.html';
+    }
+});
